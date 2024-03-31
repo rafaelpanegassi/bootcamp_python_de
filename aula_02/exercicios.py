45b567b166c90922ec04b9d0a54fb2b50d14fe6e
Rafael Panegassi <70807807+rafaelpanegassi@users.noreply.github.com>
2024-03-30 23:17:02 -0300

    Feito ate exercicio 10

--- aula_02/exercicios.py
+++ aula_02/exercicios.py
@@ -30,31 +30,63 @@ print(valor1 != valor2)
 #### Inteiros (`int`)
 #1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
 # %%
-valor1 = int(input('Digite um primeiro valor: '))
-valor2 = int(input('Digite um primeiro valor: '))
-print(valor1+valor2)
+num1 = int(input("Digite o primeiro número inteiro: "))
+num2 = int(input("Digite o segundo número inteiro: "))
+resultado_soma = num1 + num2
+print("A soma é:", resultado_soma)
 #2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
 # %%
-valor1 = int(input('Digite um primeiro valor: '))
-print(valor1 % 5)
+num = int(input("Digite um número: "))
+resultado_resto = num % 5
+print("O resto da divisão por 5 é:", resultado_resto)
 #3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
 # %%
-valor1 = int(input('Digite um primeiro valor: '))
-valor2 = int(input('Digite um primeiro valor: '))
-print(valor1*valor2)
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+resultado_multiplicacao = num1 * num2
+print("O resultado da multiplicação é:", resultado_multiplicacao)
 #4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
 # %%
-valor1 = int(input('Digite um primeiro valor: '))
-valor2 = int(input('Digite um primeiro valor: '))
-print(valor1//valor2)
+num1 = int(input("Digite o primeiro número inteiro: "))
+num2 = int(input("Digite o segundo número inteiro: "))
+resultado_divisao_inteira = num1 // num2
+print("O resultado da divisão inteira é:", resultado_divisao_inteira)
 #5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+# %%
+num = int(input("Digite um número: "))
+resultado_quadrado = num ** 2
+print("O quadrado do número é:", resultado_quadrado)
 
 #### Números de Ponto Flutuante (`float`)
 #6. Escreva um programa que receba dois números flutuantes e realize sua adição.
+# %%
+num1 = float(input("Digite o primeiro número flutuante: "))
+num2 = float(input("Digite o segundo número flutuante: "))
+resultado_soma = num1 + num2
+print("A soma é:", resultado_soma)
 #7. Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.
+# %%
+num1 = float(input("Digite o primeiro número flutuante: "))
+num2 = float(input("Digite o segundo número flutuante: "))
+media = (num1 + num2) / 2
+print("A média é:", media)
 #8. Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
+# %%
+base = float(input("Digite a base: "))
+expoente = float(input("Digite o expoente: "))
+potencia = base ** expoente
+print("O resultado da potência é:", potencia)
 #9. Faça um programa que converta a temperatura de Celsius para Fahrenheit.
+# %%
+celsius = float(input("Digite a temperatura em Celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+print(f"{celsius}°C é igual a {fahrenheit}°F")
 #10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
+# %%
+import math
+raio = float(input("Digite o raio do círculo: "))
+area = math.pi * raio ** 2
+print("A área do círculo é:", f'{area:.2f}')
 
 #### Strings (`str`)
 #11. Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
