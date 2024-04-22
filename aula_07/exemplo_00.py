0fff7f56a84744c4339d4e5463958385a89c4bf1
Rafael Panegassi <70807807+rafaelpanegassi@users.noreply.github.com>
2024-04-22 17:16:58 -0300

    exercicio aula 07

---
+++ aula_07/exemplo_00.py
@@ -0,0 +1,18 @@
+# %%
+print('Olá, este é meu workspace')
+# %%
+def soma(valor1_para_somar: float,valor2_para_somar: float) -> float:
+    """
+    Uma função simples de soma de valores do tipo float que retorna float
+    """
+    resultado_soma = valor1_para_somar+valor2_para_somar
+    return resultado_soma
+
+v1 = 55.5
+v2 = 77.3
+
+soma_01 = soma(5,8.8)
+soma_02 = soma(valor1_para_somar=v1,valor2_para_somar=v2)
+
+print(soma_01)
+print(soma_02)
