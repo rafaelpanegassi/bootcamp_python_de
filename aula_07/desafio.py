fc0f36cdea96eb15afafabf4191cf45b5cd9809b
Rafael Panegassi <70807807+rafaelpanegassi@users.noreply.github.com>
2024-04-22 22:03:51 -0300

    Finalizando aula 07 completa

--- aula_07/desafio.py
+++ aula_07/desafio.py
@@ -1,8 +1,8 @@
-#Desafio: Análise de Vendas de Produtos
-#Objetivo: Dado um arquivo CSV contendo dados de vendas de produtos, o desafio consiste em ler os dados, processá-los em um dicionário para análise e, por fim, calcular e reportar as vendas totais por categoria de produto.
+# Desafio: Análise de Vendas de Produtos
+# Objetivo: Dado um arquivo CSV contendo dados de vendas de produtos, o desafio consiste em ler os dados, processá-los em um dicionário para análise e, por fim, calcular e reportar as vendas totais por categoria de produto.
 
 # %%
-from etl_type_hint import ler_csv,processar_dados,calcular_vendas_categoria
+from etl_type_hint import calcular_vendas_categoria, ler_csv, processar_dados
 
 data = ler_csv("data/vendas.csv")
 print(data)
@@ -10,4 +10,4 @@ processed_data = processar_dados(data)
 print(processed_data)
 sales_data = calcular_vendas_categoria(processed_data)
 for categoria, total in processed_data.items():
-        print(f'{categoria}: ${total}')
+    print(f"{categoria}: ${total}")
--- aula_07/etl_pydantic.py
+++ aula_07/etl_pydantic.py
@@ -1,6 +1,8 @@
-from pydantic import BaseModel, field_validator, ValidationError
-from typing import List, Dict, Optional
 import csv
+from typing import Dict, List, Optional
+
+from pydantic import BaseModel, ValidationError, field_validator
+
 
 class ItemVenda(BaseModel):
     Produto: str
@@ -9,10 +11,11 @@ class ItemVenda(BaseModel):
     Venda: int
 
     # Validador para garantir valores positivos
-    @field_validator('Quantidade', 'Venda')
+    @field_validator("Quantidade", "Venda")
     def valores_positivos(cls, v):
-        assert v >= 0, 'deve ser positivo'
+        assert v >= 0, "deve ser positivo"
         return v
+
 
 class CategoriaDados(BaseModel):
     Categoria: str
@@ -22,9 +25,10 @@ class CategoriaDados(BaseModel):
     def calcular_total_vendas(self):
         self.TotalVendas = sum(item.Quantidade * item.Venda for item in self.Itens)
 
+
 def ler_csv(nome_arquivo: str) -> List[ItemVenda]:
     dados_validados = []
-    with open(nome_arquivo, mode='r', encoding='utf-8') as arquivo:
+    with open(nome_arquivo, mode="r", encoding="utf-8") as arquivo:
         leitor = csv.DictReader(arquivo)
         for linha in leitor:
             try:
@@ -34,13 +38,17 @@ def ler_csv(nome_arquivo: str) -> List[ItemVenda]:
                 print(f"Erro de validação: {e.json()}")
     return dados_validados
 
+
 def processar_dados(dados: List[ItemVenda]) -> Dict[str, CategoriaDados]:
     categorias = {}
     for item in dados:
         if item.Categoria not in categorias:
-            categorias[item.Categoria] = CategoriaDados(Categoria=item.Categoria, Itens=[])
+            categorias[item.Categoria] = CategoriaDados(
+                Categoria=item.Categoria, Itens=[]
+            )
         categorias[item.Categoria].Itens.append(item)
     return categorias
+
 
 def calcular_vendas_categoria(dados: Dict[str, CategoriaDados]) -> Dict[str, int]:
     vendas_por_categoria = {}
@@ -49,13 +57,15 @@ def calcular_vendas_categoria(dados: Dict[str, CategoriaDados]) -> Dict[str, int
         vendas_por_categoria[categoria] = dados_categoria.TotalVendas
     return vendas_por_categoria
 
+
 def main():
-    nome_arquivo = 'vendas.csv'
+    nome_arquivo = "vendas.csv"
     dados_brutos = ler_csv(nome_arquivo)
     dados_processados = processar_dados(dados_brutos)
     vendas_categoria = calcular_vendas_categoria(dados_processados)
     for categoria, total in vendas_categoria.items():
-        print(f'{categoria}: ${total}')
+        print(f"{categoria}: ${total}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
